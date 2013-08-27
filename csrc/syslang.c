b2fcbf3f0f97f4cede895c7eb94fb5ddae8f0fb2
Breckin Loggins <bloggins@kabam.com>
2013-08-27 13:22:39 -0700

    stack semantics (doesn't work yet)

--- csrc/syslang.c
+++ csrc/syslang.c
@@ -6,6 +6,7 @@
 #define DIE(s) fprintf(stderr, "ERROR: %s\n", s), exit(1);
 
 #define MAX_WORD_LEN  32
+#define STACK_SIZE    1024
 
 struct list_node_t {
   struct list_node_t* next;
@@ -19,6 +20,13 @@ size_t list_count(struct list_node_t* list)
   return count;
 }
 
+struct word_trie_node_t;
+struct word_ptr_node_t {
+  struct list_node_t list_node;
+
+  struct word_trie_node_t* word;
+} word_ptr_node;
+
 enum node_type_t {
   node_fragment = 0,          /* Part of a word only */
   node_normal,                /* A normal word (TODO: What does this mean?) */
@@ -31,14 +39,16 @@ struct word_trie_node_t {
   int words;          /* Number of words terminating here */
   int prefixes;       /* Number of words for which this is a prefix */
 
+  /* TODO: Should actually be a list so FORGET will work (or will envs handle
+   * it?)
+   */
   enum node_type_t type;
   int immediate;      /* If a non-fragment node, whether this node executes 
                          immediately regardless of interpreter mode
                       */
 
-  union {
-    void (*code)(struct env_t* env);
-  } content;
+  void (*code)(struct env_t* env);
+  struct word_ptr_node_t* param;
 
   struct word_trie_node_t* edges[128]; /* This is obviously inefficient and won't work for unicode */
 } word_trie_node;
@@ -121,7 +131,7 @@ void word_trie_print(struct word_trie_node_t* start, int level, FILE* fp)
 
 int word_trie_count_all_words(struct word_trie_node_t* start)
 {
-  int words = start->words;
+  int words = start->words > 0 ? 1 : 0; /* Don't count redefines */
 
   for (int i = 0; i < 128; i++) {
     if (!start->edges[i]) continue;
@@ -141,8 +151,6 @@ struct word_trie_node_t* word_trie_lookup(struct word_trie_node_t* start, char *
      return 0; 
   }
 
-  if (word_count > 1) DIE("internal compiler error - invalid word count"); /* Sanity check */
-
   return word_node;
 }
 
@@ -155,17 +163,25 @@ struct env_t {
   struct env_t* parent;
   enum interpreter_mode_t mode;
   
+  struct word_trie_node_t* ip;  /* instruction pointer */
   char cur_word[MAX_WORD_LEN + 1];
   int cur_word_idx;
 
   struct word_trie_node_t* dictionary;
+  
+  struct word_trie_node_t* stack[STACK_SIZE];
+  int stack_idx;
+
+  struct word_trie_node_t* rstack[STACK_SIZE];
+  int rstack_idx;
 };
 
 struct env_t* env_new(struct env_t* parent)
 {
   struct env_t* env = calloc(1, sizeof(struct env_t));
   env->parent = parent;
-
+  env->stack_idx = STACK_SIZE - 1;
+  env->rstack_idx = STACK_SIZE - 1;
   return env;
 }
 
@@ -193,25 +209,14 @@ struct word_trie_node_t* env_lookup(struct env_t* env, char* word)
   }
 }
 
-void builtin__colon_(struct env_t* env)
-{
-  /* TODO: parse name, enter compilation state */
-  fprintf(stderr, "COLON\n");
-}
-
-void builtin__semicolon_(struct env_t* env)
-{
-  fprintf(stderr, "SEMICOLON\n");
-}
-
 void builtin_bye(struct env_t* env)
 {
   exit(0);
 }
 
-void builtin__dbg(struct env_t* env)
+void builtin_words(struct env_t* env)
 {
-  word_trie_print(env->dictionary, 0, stderr);
+  /* TODO: implement when we have iterator and args */
 }
 
 void builtin_read_word(struct env_t* env)
@@ -234,30 +239,100 @@ void builtin_read_word(struct env_t* env)
   if (ch == EOF) builtin_bye(env);
 }
 
+void builtin_execute(struct env_t* env)
+{
+  if (!env->ip) DIE("internal compiler error - no instruction pointer");
+
+  if (env->mode == imode_interpret || env->ip->immediate) {
+    env->ip->code(env);
+  } else if (env->mode == imode_compile) {
+    struct word_ptr_node_t* last = env->ip->param;
+    while (last && last->list_node.next) last = (struct word_ptr_node_t*)last->list_node.next;
+
+    last = calloc(1, sizeof(struct word_ptr_node_t));
+    if (!env->ip->param) env->ip->param = last;
+
+    last->word = env->ip;
+  } else {
+    DIE("internal compiler error - unimplemented compiler mode");
+  }
+}
+
+void builtin_call(struct env_t* env)
+{
+  /* like execute but saves the call stack */
+  if (env->rstack_idx == 0) DIE("stack overflow");
+  if (env->rstack_idx >= STACK_SIZE) DIE("stack underflow");
+
+  env->rstack[--env->rstack_idx] = env->ip;
+  struct word_ptr_node_t* p = env->ip->param;
+  while (p) {
+    fprintf(stderr, "HUH?\n");
+    env->ip = p->word;
+    builtin_execute(env);
+    p = (struct word_ptr_node_t*)p->list_node.next;
+  }
+  
+  if (env->rstack_idx >= STACK_SIZE) DIE("stack underflow");
+  env->ip = env->rstack[env->rstack_idx++];
+}
+
+void builtin__colon_(struct env_t* env)
+{
+  builtin_read_word(env);
+  env->ip = env_add(env, env->cur_word);
+  env->ip->type = node_normal;
+  env->ip->code = builtin_call;
+  env->mode = imode_compile;
+}
+
+void builtin__semicolon_(struct env_t* env)
+{
+  env->ip = 0; /* TODO: Restore rstack */
+  env->mode = imode_interpret;
+}
+
+void builtin__dbg(struct env_t* env)
+{
+  word_trie_print(env->dictionary, 0, stderr);
+}
+
 void root_env_init(struct env_t* env)
 {
   struct word_trie_node_t* node = 0;
   
   node = env_add(env, ":");
   node->type = node_code;
-  node->content.code = builtin__colon_;
+  node->code = builtin__colon_;
 
   node = env_add(env, ";");
   node->type = node_code;
   node->immediate = 1;
-  node->content.code = builtin__semicolon_;
+  node->code = builtin__semicolon_;
+
+  node = env_add(env, "execute");
+  node->type = node_code;
+  node->code = builtin_execute;
+
+  node = env_add(env, "call");
+  node->type = node_code;
+  node->code = builtin_call;
   
   node = env_add(env, "_dbg");
   node->type = node_code;
-  node->content.code = builtin__dbg;
+  node->code = builtin__dbg;
+
+  node = env_add(env, "words");
+  node->type = node_code;
+  node->code = builtin_words;
 
   node = env_add(env, "read_word");
   node->type = node_code;
-  node->content.code = builtin_read_word;
+  node->code = builtin_read_word;
 
   node = env_add(env, "bye"); 
   node->type = node_code;
-  node->content.code = builtin_bye; 
+  node->code = builtin_bye; 
 }
 
 int main(int argc, char** argv)
@@ -276,9 +351,8 @@ int main(int argc, char** argv)
     builtin_read_word(cur_env);
     struct word_trie_node_t* trie_node = env_lookup(cur_env, cur_env->cur_word);
     if (trie_node)  {
-      if (trie_node->type == node_code) {
-        trie_node->content.code(cur_env);
-      }
+      cur_env->ip = trie_node;
+      builtin_execute(cur_env);
     } else {
       fprintf(stderr, "Error: %s is undefined\n", cur_env->cur_word);
     }
